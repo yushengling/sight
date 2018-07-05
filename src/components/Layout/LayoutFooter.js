f4c4822bdf194fb3ad350bf7e912d834e0ed11e6
xuya227939 <1245244711@qq.com>
2018-07-05 14:19:09 +0800

    新增备案信息

--- src/components/Layout/LayoutFooter.js
+++ src/components/Layout/LayoutFooter.js
@@ -4,7 +4,7 @@ import * as styles from './LayoutFooter.css';
 function LayoutFooter(props) {
   return (
     <footer className="layoutfooter" >
-      © 2018 视线
+      © 2018 视线 <a className="layoutfooter-a" href="http://www.miitbeian.gov.cn/">备案号：湘ICP备18013456号</a>
     </footer>
   )
 }
