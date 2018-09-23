5aa69b3b43a7d4c23025ab7ccd81ab44416590f9
xuya227939 <1245244711@qq.com>
2018-09-23 21:18:15 +0800

    响应式布局和增加注册的输入条件判断

--- src/components/User/UserRegister.js
+++ src/components/User/UserRegister.js
@@ -13,8 +13,12 @@ class UserRegister extends React.Component {
   
     this.state = {
       formItems: ['phone|手机号|0', 'user|用户名|0', 'password|密码|0'],
+      tips: '',
     };
     this.numbers = [];
+    this.phone = 0;
+    this.user = '';
+    this.password = '';
   }
   componentWillReceiveProps(nextProps) {
     const { userRedu } = nextProps;
@@ -30,12 +34,39 @@ class UserRegister extends React.Component {
   }
   handleSubmit = (e) => {
     e.preventDefault();
-    const { form, dispatch } = this.props;
-    form.validateFieldsAndScroll((err, values) => {
-      if (!err) {
-        register(dispatch, values);
-      }
-    });
+    const { dispatch } = this.props;
+    const phonePattern = /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/;
+    const namePattern = /^\d/;
+    let tips = '';
+    if(!(phonePattern.test(this.phone)) || !this.phone) {
+      tips = '手机号输入不正确';
+      this.setState(() => ({
+        tips
+      }));
+      return;
+    }
+    if(namePattern.test(this.user) || !this.user) {
+      tips = '用户名输入不正确';
+      this.setState(() => ({
+        tips
+      }));
+      return;
+    }
+    if(!this.password || !this.password) {
+      tips = '密码输入不正确';
+      this.setState(() => ({
+        tips
+      }));
+      return;
+    }
+    this.setState(() => ({
+      tips
+    }));
+    let datas = {};
+    datas.phone = this.phone;
+    datas.user = this.user;
+    datas.password = this.password;
+    // register(dispatch, datas);
   }
   getNumbers(value) {
     this.numbers = value;
@@ -43,16 +74,17 @@ class UserRegister extends React.Component {
   inputHanlder = (id, e) => {
     let value = e.target.value;
     let { formItems } = this.state;
-    console.log(value);
     switch(id) {
       case 0:
         const phonePattern = /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/;
         if(!value) {
+          this.phone = value;
           formItems[0] = formItems[0].replace(/[0-9]/, 2);
           break;
         }
         if(phonePattern.test(value)) {
           formItems[0] = formItems[0].replace(/[0-9]/, 1);
+          this.phone = value;
         } else {
           formItems[0] = formItems[0].replace(/[0-9]/, 2);
         }
@@ -60,14 +92,25 @@ class UserRegister extends React.Component {
       case 1:
         const namePattern = /^\d/;
         if(!value) {
+          this.user = value;
           formItems[1] = formItems[1].replace(/[0-9]/, 2);
           break;
         }
         if(!(namePattern.test(value))) {
           formItems[1] = formItems[1].replace(/[0-9]/, 1);
+          this.user = value;
         } else {
           formItems[1] = formItems[1].replace(/[0-9]/, 2);
         }
+      break;
+      case 2:
+        if(!value) {
+          this.password = value;
+          formItems[2] = formItems[2].replace(/[0-9]/, 2);
+          break; 
+        }
+        formItems[2] = formItems[2].replace(/[0-9]/, 1);
+        this.password = value;
       break;
     }
     this.setState(() => ({
@@ -80,7 +123,6 @@ class UserRegister extends React.Component {
     const { formItems } = this.state;
     formItemsArray = formItems.map((list, id) => {
       let validateStatus, hasFeedback;
-      console.log(parseInt(list.split('|')[2]));
       if(parseInt(list.split('|')[2]) === 1) {
         hasFeedback = true;
         validateStatus = 'success';
@@ -97,13 +139,20 @@ class UserRegister extends React.Component {
     return formItemsArray;
   }
   render() {
+    const { tips } = this.state;
     return (
       <Form onSubmit={this.handleSubmit} className="login-form">
         <h2 className="register-title">注册视线，进入你的世界</h2>
         {this.renderFormItem()}
         <FormItem>
-          <Button type="primary" htmlType="submit" className="login-form-button">注册</Button>
+          <Button type="primary" htmlType="submit" className="login-form-button user-register">注册</Button>
         </FormItem>
+        <p className="register-clause">
+          注册即表示你同意接受我们的<a target="_blank" href="">条款</a> 、 <a target="_blank" href="">数据使用政策</a>
+        </p>
+        <p className="register-text-tips">
+          {tips}
+        </p>
       </Form>
     );
   }
