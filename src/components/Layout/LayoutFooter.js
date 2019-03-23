4400ad8a76853569a61893d11ccf3043439d2e4b
xuya227939 <hezhiqianye@gmail.com>
2019-03-23 11:11:21 +0800

    Add footer github icon

--- src/components/Layout/LayoutFooter.js
+++ src/components/Layout/LayoutFooter.js
@@ -2,8 +2,14 @@ import React,{ Component } from 'react';
 import * as styles from './LayoutFooter.less';
 function LayoutFooter() {
   return (
-    <footer className="layoutfooter" >
-      © 2019 corki-ui <a className="layoutfooter-a" href="http://www.miitbeian.gov.cn/">备案号：湘ICP备18013456号</a>
+    <footer className="layoutfooter">
+      <p>
+        <a className="layoutfooter-icon icon-github" href="https://github.com/xuya227939/corki-ui" target="_blank">
+        </a>
+      </p>
+      <p>
+        Copyright © 2019 Liu Jiang {/*<a className="layoutfooter-a" href="http://www.miitbeian.gov.cn/">备案号：湘ICP备18013456号</a>*/}
+      </p>
     </footer>
   )
 }
